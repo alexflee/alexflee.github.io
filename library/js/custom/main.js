6deb8686a72c861c8192dfd50d27b58e9f651073
alex.lee1 <alex.lee@impression.co.uk>
2020-08-07 15:27:11 +0100

    update style
    update

--- library/js/custom/main.js
+++ library/js/custom/main.js
@@ -1,25 +1,5 @@
 
-$(window).on('scroll', function() {
-    var scrollTop = $(this).scrollTop();
 
-    $('.to-fade-in').each(function() {
-      var topDistance = $(this).offset().top;
-      if ( (topDistance-500) < scrollTop ) {
-        $(this).addClass('fade-in');
-      }
-    });
-
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 50) {
-        $(".header").addClass("header--reduce");
-    } else {
-        $(".header").removeClass("header--reduce");
-    }
-
-});
-
-function initMap() {}
 
 $(document).ready(function(){
 
@@ -31,10 +11,17 @@ $(document).ready(function(){
 
 
     //mobile nav switch
-    $('#nav-icon').click(function(){
-        $(this).toggleClass('open');
-        $('.main-nav__navigation').toggleClass('main-nav__navigation--active');
-        $('.main-nav').toggleClass('add-bg');
+    $('#information').click(function(){
+        $('.information-more').toggleClass('active');
+        $(this).toggleClass('active');
+
+        if ($(this ).hasClass( "active" )){
+          $(this).attr('aria-expanded', 'true');
+          $('.information-more').data('hidden', 'false');
+        }else{
+          $(this).attr('aria-expanded', 'false');
+          $('.information-more').data('hidden', 'true');
+        }
     });
 
     //open sub nav
@@ -43,47 +30,5 @@ $(document).ready(function(){
             $(this).parent().find('.mobile-nav').toggleClass('mobile-nav--active');
         }
     });
-
-    //mobile hide classes
-    function screenClass() {
-        if($(window).innerWidth() > 768) {
-            $('.main-nav__navigation').removeClass('main-nav__navigation--active');
-            $('.main-nav').removeClass('add-bg');
-            $('#nav-icon').removeClass('open');
-            $('.nav--plus').removeClass('open-nav');
-            $('.mobile-nav').removeClass('mobile-nav--active');
-        }else{
-            $('.nav--plus').addClass('open-nav');
-        }
-    }
-
-    // Fire.
-    screenClass();
-
-    // And recheck if window gets resized.
-    $(window).bind('resize',function(){
-        screenClass();
-    });
-
-    if($('#map').length){
-
-      //google maps
-      var map;
-      function initMap() {
-        var center = {lat: 53.820480, lng: -1.251990};
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 53.820480, lng: -1.251990},
-          zoom: 9
-        });
-        var marker = new google.maps.Marker({position: center, map: map});
-      }
-
-      window.initMap = initMap;
-
-    }else{
-      function initMap() {}
-    }
-
-
 
 });
