a2debc1b079e59314fa3761cb48c9e44a3f544d9
alex.lee1 <alex.lee@impression.co.uk>
2023-07-27 16:11:54 +0100

    website updat
    big website update test push

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+  $('#toggle').addClass('active');
+
+  const btn = document.querySelector("#toggle-theme-colour");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const currentTheme = localStorage.getItem("theme");
+  if (currentTheme == "dark") {
+    document.body.classList.toggle("dark-theme");
+  } else if (currentTheme == "light") {
+    document.body.classList.toggle("light-theme");
+  }
+
+  btn.addEventListener("click", function () {
+    if (prefersDarkScheme.matches) {
+      document.body.classList.toggle("light-theme");
+      var theme = document.body.classList.contains("light-theme")
+        ? "light"
+        : "dark";
+    } else {
+      document.body.classList.toggle("dark-theme");
+      var theme = document.body.classList.contains("dark-theme")
+        ? "dark"
+        : "light";
+    }
+    localStorage.setItem("theme", theme);
+
+    $('#toggle-change-type').html(theme);
+
+  });
+
+
+});
+
+
